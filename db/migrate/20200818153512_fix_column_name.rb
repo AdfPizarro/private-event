6063cd117d4148039a1b3a88038c1dc3d1096d04
Adrian <adfpizarro@gmail.com>
2020-08-18 09:48:54 -0600

    Rename the column names in the attendees table

---
+++ db/migrate/20200818153512_fix_column_name.rb
@@ -0,0 +1,6 @@
+class FixColumnName < ActiveRecord::Migration[6.0]
+  def change
+   rename_column :attendees, :user_id, :attendee
+   rename_column :attendees, :event_id, :attended_event
+  end
+end
