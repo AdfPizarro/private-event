ea5e12043a14cbe518a65cb793b048549f4f4dd9
Adrian <adfpizarro@gmail.com>
2020-08-18 13:34:12 -0600

    Update event relationship to show the attendees

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,5 @@
 class Event < ApplicationRecord
-    has_many :attendees
+    has_many :attendees, class_name: User.name, through: :attendees
     #has_many :users, through: :attendees
     belongs_to :creator,  class_name: User.name
 end
