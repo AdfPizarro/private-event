ddf0acce71379bbb53e9b852b1cb3e75ee2f449b
Adrian <adfpizarro@gmail.com>
2020-08-28 11:11:35 -0600

    Add method to register to an event

--- app/controllers/attendees_controller.rb
+++ app/controllers/attendees_controller.rb
@@ -1,7 +1,20 @@
 class AttendeesController < ApplicationController
  def create
+      #puts params[:event_id]
+       @attendee= current_user.attendees.build
+       @attendee.attended_event_id=params[:event_id]
+       
+       
+      respond_to do |format|
+        if @attendee.save
+            format.html { redirect_to event_path(@attendee.attended_event_id), notice: 'Attendee confirmed' }
+            format.json { render :show, status: :created, location: @attendee }
+        else
+            format.html { render :new }
+            format.json { render json: @attendee.errors, status: :unprocessable_entity }     
+        end
+      end
 
-       @event= current_user.events.build
   end
 
 end
