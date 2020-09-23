6a0a9f43f774a8308845a50caf23154d08082912
Adrian <adfpizarro@gmail.com>
2020-09-23 15:04:39 -0600

    Split long query

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -4,10 +4,8 @@ class EventsController < ApplicationController
   def index
     @upcoming_events = Event.upcoming
     @past_events = Event.past
-
-    @events = Event.find_by_sql('SELECT "events".* FROM "events" INNER JOIN "attendees" ON "events"."id" =
-                                "attendees"."attended_event_id" WHERE NOT "attendees"."attendee_id" = ' 
-				+ current_user.id.to_s)
+    query=SELECT "events".* FROM "events" INNER JOIN "attendees" ON "events"."id" = "attendees"."attended_event_id" WHERE NOT "attendees"."attendee_id" = '
+    @events = Event.find_by_sql(query + current_user.id.to_s)
     @appointments = current_user.appointments
   end
 
