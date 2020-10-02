c5165e671d088d7c9df1d40b91d4b47bc953c7a7
Adrian <adfpizarro@gmail.com>
2020-10-02 13:58:31 -0600

    Add test to validate event relationship

--- Gemfile
+++ Gemfile
@@ -42,6 +42,7 @@ group :development do
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
   gem 'spring-watcher-listen', '~> 2.0.0'
+  gem 'database_cleaner'
 end
 
 group :test do
@@ -49,6 +50,7 @@ group :test do
   gem 'capybara', '>= 2.15'
   gem 'selenium-webdriver'
   # Easy installation and use of web drivers to run system tests with browsers
+  gem 'shoulda-matchers'
   gem 'webdrivers'
 end
 
---
+++ spec/models/event_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.describe Event, type: :model do
+  describe 'associations' do
+    it { should belong_to(:creator).class_name(User.name) }
+    it { should have_many(:attendees).with_foreign_key(:attended_event_id) }
+    it { should have_many(:invitees).through(:attendees).source(:attendee) }
+  end
+
+  describe 'validations' do
+    subject do
+      Event.create({
+                     name: 'A random user',
+                     location: 'foobar',
+                     description: 'lorem ipsum dolor sit amet',
+                     date: Date.tomorrow,
+                     creator_id: 1
+                   })
+    end
+
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:location) }
+    it { should validate_presence_of(:description) }
+    it { should validate_presence_of(:date) }
+  end
+end
