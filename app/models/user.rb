0607f066f65abbfff98c3db2942208d7e48ac794
Adrian <adfpizarro@gmail.com>
2020-10-02 14:15:13 -0600

    Add validator to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ApplicationRecord
   has_many :events, foreign_key: :creator_id
   has_many :attendees, foreign_key: :attendee_id
   has_many :appointments, through: :attendees, source: :attended_event
+
+ validates :name, :username, presence: true
 end
